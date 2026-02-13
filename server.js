8374fd9ab41f76976ea62f92f56d9ee358f6b06d
Mayurrr02 <mayurjadhav1266@gmail.com>
2026-02-13 19:32:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,10 @@ if (!process.env.MONGO_URI) {
 }
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected Successfully"))
-.catch(err => {
-    console.error("MongoDB Connection Error:", err.message);
-});
+.catch(err => console.error("MongoDB Connection Error:", err.message));
+
 
 
 // Routes
